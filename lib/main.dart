f261f00205678e1e36937b84cf6cd45ba0e7f03e
RashmiRanganathan <rashmi05rash@gmail.com>
2021-03-25 13:47:00 +0530

    Remove filed unrealted to local database

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:local_storage/data/local_database/friends_local_datasource.dart';
-import 'package:local_storage/data/shared_preferences/local_preferences.dart';
 import 'package:local_storage/presentation/app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  await LocalPreferences().init();
 
   await FriendsLocalDatasource().init();
 
